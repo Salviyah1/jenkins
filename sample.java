bb75c11b614c2992d58c0537459e5c14b3e90b42
Salviyah1 <salviyah.b@gmail.com>
2024-11-17 10:19:01 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class simple
   {
     public static void main(String args[]) throws InterruptedException
     {
-      Threads.sleep(100000);
+      Thread.sleep(100000);
       System.out.println("Hello Java");
     }
   }
