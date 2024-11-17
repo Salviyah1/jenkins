42d56e1247df76351d8e39db2edf7cddedc261fa
Salviyah1 <salviyah.b@gmail.com>
2024-11-17 10:13:59 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+class simple
+  {
+    public static void main(String args[]) throws InterruptedException
+    {
+      Threads.sleep(100000);
+      System.out.println("Hello Java");
+    }
+  }
+    
